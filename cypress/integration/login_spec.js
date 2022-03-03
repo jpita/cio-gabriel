4beb16e7d0c1e9b4f1c3c8b2916c796b3fe64ad9
gabriel pita <dvpita@gmail.com>
2022-03-03 23:39:56 +0000

    Update login_spec.js

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -4,12 +4,11 @@ describe('Login tests', () => {
     it('login successful', () => {
       cy.visit("https://fly.customer.io/")
       cy.get('.signup-title').should('exist')
-      cy.get('.fly-form-control').type('YOUR_EMAIL_HERE')
+      cy.get('.fly-form-control').type(Cypress.env('EMAIL'))
       cy.get('.mt-2 > .fly-flex > .fly-btn').click()
       cy.get('.jc-end > .fly-btn').should('exist')
-      cy.get('#ember9 > input').type('YOUR_PASSWORD_HERE')
+      cy.get('#ember9 > input').type(Cypress.env('PASSWORD'))
       cy.get('.jc-end > .fly-btn').click()
       cy.get('.nav-workspace-dropdown-logo').should('exist')
     })
-
   })
