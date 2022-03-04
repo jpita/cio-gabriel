1b421cac2473e66b5d21bd4db6d5aa15f2e7d82b
gabriel pita <dvpita@gmail.com>
2022-03-04 00:09:39 +0000

    add login fail wrong password test

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -11,4 +11,15 @@ describe('Login tests', () => {
       cy.get('.jc-end > .fly-btn').click()
       cy.get('.nav-workspace-dropdown-logo').should('exist')
     })
+
+    it('login fail wrong password', () => {
+        cy.visit("https://fly.customer.io/")
+        cy.get('.signup-title').should('exist')
+        cy.get('.fly-form-control').type('jnsjsn@gmailcom')
+        cy.get('.mt-2 > .fly-flex > .fly-btn').click()
+        cy.get('.jc-end > .fly-btn').should('exist')
+        cy.get('#ember9 > input').type('skjhskjsh')
+        cy.get('.jc-end > .fly-btn').click()
+        cy.get('.button-link > .fly-svg-icon > .fly-svg-icon__icon > path').should('exist')
+      })
   })
